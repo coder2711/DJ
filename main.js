e9ba918a337b3a994fab60823207fa2ffe25a033
coder2711 <73233141+coder2711@users.noreply.github.com>
2022-01-17 20:00:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ Score_left = 0;
 Score_right = 0;
  function preload(){
      music_1 = loadSound("MissionImpossible.mp3");
-     music_2 = loadSound("flute.mp3");
+     music_2 = loadSound("a.mp3");
  }
 
  function setup(){
@@ -13,37 +13,42 @@ Score_right = 0;
     vidau = createCapture(VIDEO);
     vidau.hide();
     posenet = ml5.poseNet(vidau, loaded );
-    posenet.on('pose',  got_Results)
+    posenet.on('pose',  got_Results);
 }
 function loaded(){
     console.log("Loaded");
 }
 
-function got_Results(reet){
-    Score_left = reet[0].pose.keypoints[9].score;
-    Score_right = reet[0].pose.keypoints[10].score;
+function got_Results(ree){
+    Score_left = ree[0].pose.keypoints[9].score;
+    Score_right = ree[0].pose.keypoints[10].score;
+    console.log(ree);
+    
 }
+
 
 function draw(){
     image(vidau ,0,0, 400 , 400);
+
     if(Score_left >0.2){
-           document.getElementById("Name").innerHTML = "The Song Playing Is - Theme Song Of Mission Impossible";
-           music_1.pause();
-           music_2.pause(); 
-           music_1.play();
-            music_1.setVolume(1);
-            music_1.rate(1);
-        
-    }
-    else if(Score_right >0.2){
-        document.getElementById("Name").innerHTML = "The Song Playing Is - Theme Song Of Harry Potter";
+        document.getElementById("Name").innerHTML = "The Song Playing Is - Theme Song Of Mission Impossible";
         music_1.pause();
-           music_2.pause();
-        music_2.play();
-        music_2.setVolume(1);
-        music_2.rate(1);
-    
+        music_2.pause(); 
+        music_1.play();
+         music_1.setVolume(1);
+         music_1.rate(1);
+     
+ }
+ else if(Score_right >0.2){
+     document.getElementById("Name").innerHTML = "The Song Playing Is - Happy Birthday Song";
+     music_1.pause();
+        music_2.pause();
+     music_2.play();
+     music_2.setVolume(1);
+     music_2.rate(1);
+ 
 }
+    
 }
 
 function stop(){
